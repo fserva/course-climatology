4b151c5ab9d1bf93f0b560b92ddad18970386b6c
fserva <federico.serva@gmail.com>
2020-03-05 14:38:24 +0100

    added environment checker script

---
+++ env_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Run this script with: `python env_test.py`
+
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+#import cartopy.crs as ccrs 
+
+x = np.arange(10)
+y = np.arange(10)*2
+
+plt.figure()
+plt.plot(x,y,color='black')
+plt.show()
+ 
+
+
+
